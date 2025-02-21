33b78511e3999f2625f44677125e9139c2a003da
nicklin99 <authen@qq.com>
2025-02-21 17:45:17 +0800

    examples build

--- packages/examples/vite.config.ts
+++ packages/examples/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-})
+  build: {
+    rollupOptions: {
+      external: ["vue", "tdesign-vue-next", "tdesign-pure"],
+    },
+  },
+});
